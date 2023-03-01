eb5635457bb418881c48ec23b89c39e04d9b50c7
Spica <spicaofsky@gmail.com>
2023-02-28 17:30:50 -0700

    Update or.py

--- z3/or.py
+++ z3/or.py
@@ -10,7 +10,7 @@ s = Solver()
 def getAbsValue(name):
     zeros=BitVec('zeros'+name,WIDTH)
     ones=BitVec('ones'+name,WIDTH)
-    s.add(zeros & ones ==0)
+    s.add(zeros & ones == 0)
     return (zeros,ones)
 
 X=getAbsValue('X')
@@ -22,29 +22,55 @@ def getInstanceFromAbsValue(name,absValue):
     s.add(contains(~inst,absValue[0]))
     return inst
 
-instX=getInstanceFromAbsValue('insX',X)
-instY=getInstanceFromAbsValue('insY',Y)
-
 def g(absX,absY):
     return (absX[0]&absY[0],absX[1]|absY[1])
 
+#This is a binary operator eliminating all known information
+#Sound but not precise
+allZeros=BitVec('allZeros',WIDTH)
+s.add(allZeros==0)
+def allToBottom(absX,absY):
+    return (allZeros,allZeros)
+
+#returns a constant ones(absX) | ones(absY)
+#precise but not sound
+def allToConstant(absX,absY):
+    return (~(absX[1]|absY[1]),absX[1]|absY[1])
+
 G=g(X,Y)
+#G=allToBottom(X,Y)
+#G=allToConstant(X,Y)
 
 def soundnessCheck():
+    instX = getInstanceFromAbsValue('insX', X)
+    instY = getInstanceFromAbsValue('insY', Y)
     s.add(Not(And(
         contains(instX|instY,G[1]),
         contains(~(instX|instY),G[0])
     )))
-    print(s.check())
+    if str(s.check())=='sat':
+        print("soundness check failed!\ncounterexample:\n")
+        print(s.model())
+    else:
+        print("soundness check successfully")
 
 def precisionCheck():
-    pass
+    instG = getInstanceFromAbsValue("insG", G)
+    instX = BitVec('insX', WIDTH)
+    instY = BitVec('insY', WIDTH)
+    s.add(
+        ForAll([instX, instY],
+               Implies(And(contains(instX, X[1]),
+                           contains(~instX, X[0]),
+                           contains(instY, Y[1]),
+                           contains(~instY, Y[0])),
+                       instG != (instX | instY)))
+    )
+    if str(s.check()) == 'sat':
+        print("precision check failed!\ncounterexample:\n")
+        print(s.model())
+    else:
+        print("precision check successfully")
 
-soundnessCheck()
-
-
-
-
-
-
-
+#soundnessCheck()
+precisionCheck()
